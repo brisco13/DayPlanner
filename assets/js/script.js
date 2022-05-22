117276da0299277cd395fa84c3247b8fba0772bd
Brisco Arechederra <brisco.arch@gmail.com>
2022-05-21 18:28:53 -0600

    Added psudeocode to js file

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Make function to dymamically make div cards for time slots
+
+//make function to get current time
+
+//make function to compare time against cards - change format based off this
+
+//button function to add event to time block and save to local storage
+
+// refresh/initialization should retrieve local storage
