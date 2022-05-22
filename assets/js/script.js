53f32dba12c37dc830f87b12600603754da502db
Brisco Arechederra <brisco.arch@gmail.com>
2022-05-21 20:27:39 -0600

    Jumbotron date/time function created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
-var holder = document.getElementById("container")
+let holder = document.getElementById("container")
+let rightNow = moment()
+let dateTime = document.getElementById("currentDay")
+let calcNow = moment().unix()
 
+
+
+timeRefresh();
 // Make function to dymamically make div cards for time slots
 
 function makeCal() {
     for (let i = 9; i < 18; i++) {
-        // create new div
         let today = "2022-05-19"
         let card = document.createElement("div")
         card.setAttribute("id",i+"timeslot")
@@ -23,6 +28,7 @@ function makeCal() {
         holder.append(card)}}
 
 makeCal()
+
 //make function to get current time
 
 //make function to compare time against cards - change format based off this
@@ -30,3 +36,13 @@ makeCal()
 //button function to add event to time block and save to local storage
 
 // refresh/initialization should retrieve local storage
+
+// make jumbotron time
+function timeRefresh () {
+    let timeInterval = setInterval(function() {
+        rightNow = moment().format("MMM Do, YYYY h:m:s") 
+        calcNow = moment().unix();
+        dateTime.textContent = rightNow + "      Unix: "+ calcNow;
+        
+    }, 1000);
+}
